de6ae861cfc542b86209c7f9c0d2e4490e15a6da
mali-pir21 <okcahua@gmail.com>
2025-01-29 17:16:06 +0000

    .

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h4 className='text-center mb-2 text-lg font-Ovo'>
             Introduction</h4>
         <h2 className='text-center text-5xl font-Ovo'>
-            About Me</h2>
+            </h2>
 
             <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
                 <div className='w-80 sm:w-100 rounded-3xl max-w-none'>
